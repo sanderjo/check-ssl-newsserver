eae7a8ebb57a929bcffa8d8b3e714b5dd045b77d
Sander Jonkers <sander.jonkers+github@gmail.com>
2017-01-27 16:37:26 +0100

    Three ways of collecting newsservers

--- run-opt-python.sh
+++ run-opt-python.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+
+# Collect potential newsservers from various sources:
+# Generate from website:
+./generate-potential-newsservers.sh  > potential-ssl-newsservers.txt
+# Add what was manually added:
+cat manual-added-newsservers.txt >> potential-ssl-newsservers.txt
+# Check apache-log:
+if [ -f "/var/log/apache2/access.log" ];
+then
+    cat /var/log/apache2/access.log | grep check_newsserver  | grep -o -P '(?<=server=)[a-zA-Z0-9_\-\.]*(?= )' | sort -u >> potential-ssl-newsservers.txt
+fi
+
+# List complete. Now convert to small letters, and do a unique sort:
 cat potential-ssl-newsservers.txt | tr "[A-Z]" "[a-z]" | sort -u > temp-newsservers-combined.txt
+
 
 # If there is a "/opt/python2711/bin/python", use that (Hello, Ubuntu 14.04). Otherwise the plain python 
 
