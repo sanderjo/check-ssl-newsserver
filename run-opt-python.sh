31a76399efa2e33a21a9342f04f780132fbd9f94
Sander Jonkers <sander.jonkers+github@gmail.com>
2017-01-27 17:27:25 +0100

    automatic copy to apache directory

--- run-opt-python.sh
+++ run-opt-python.sh
@@ -2,11 +2,17 @@
 
 
 # Collect potential newsservers from various sources:
-# Generate from website:
-./generate-potential-newsservers.sh  > potential-ssl-newsservers.txt
+# The base: SSL newsservers that were found earlier and stored in the .csv file:
+if [ -f "newsservers-with-SSL.csv" ];
+then
+    cat newsservers-with-SSL.csv  | awk -F\, '{ print $1 }' | tr -d '"' > potential-ssl-newsservers.txt
+fi
+# Generate from reddit:
+./generate-potential-newsservers.sh  >> potential-ssl-newsservers.txt
 # Add what was manually added:
 cat manual-added-newsservers.txt >> potential-ssl-newsservers.txt
-# Check apache-log:
+# Check apache-log for cgi-bin lines:
+# "GET /cgi-bin/check_newsserver.py?server=newsreader.eweka.nl HTTP/1.1"
 if [ -f "/var/log/apache2/access.log" ];
 then
     cat /var/log/apache2/access.log | grep check_newsserver  | grep -o -P '(?<=server=)[a-zA-Z0-9_\-\.]*(?= )' | sort -u >> potential-ssl-newsservers.txt
@@ -16,7 +22,7 @@ fi
 cat potential-ssl-newsservers.txt | tr "[A-Z]" "[a-z]" | sort -u > temp-newsservers-combined.txt
 
 
-# If there is a "/opt/python2711/bin/python", use that (Hello, Ubuntu 14.04). Otherwise the plain python 
+# If there is a "/opt/python2711/bin/python" (Hello, Ubuntu 14.04), use that. Otherwise use plain python 
 
 if [ -f "/opt/python2711/bin/python" ];
 then
@@ -25,6 +31,18 @@ else
    cat temp-newsservers-combined.txt | awk '{ print "python check_ssl_newsserver---to-csv.py " $1 }' | /bin/sh | grep OK > newsservers-with-SSL.csv
 fi
 
+# OK, done with searching
 
+# Convert the CSV to HTML:
 cat header.csv newsservers-with-SSL.csv | csv2html > newsservers-with-SSL-noncolor.html
+# Convert "OK" to a green "OK", and "NOK" to a red "NOK":
 python color-OK-and-NOK.py < newsservers-with-SSL-noncolor.html > newsservers-with-SSL.html
+
+if [ -d "/var/www/newsservers/" ]; then
+  cp newsservers-with-SSL* /var/www/newsservers/
+fi
+
+
+
+
+
